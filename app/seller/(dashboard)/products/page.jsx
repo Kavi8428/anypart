650bb8c00327a5e2166d70ee7da38d0536567067
Kavi8428 <lakshanudara987@gmail.com>
2026-02-13 20:13:47 +0530

    fixed the session cookie issue -1

--- app/seller/(dashboard)/products/page.jsx
+++ app/seller/(dashboard)/products/page.jsx
@@ -2,6 +2,8 @@ import { getProducts } from "@/app/actions/products";
 import { ProductList } from "@/components/dashboard/products/product-list";
 import { redirect } from "next/navigation";
 
+export const dynamic = 'force-dynamic';
+
 export default async function SellerProductsPage() {
   let products;
   try {
